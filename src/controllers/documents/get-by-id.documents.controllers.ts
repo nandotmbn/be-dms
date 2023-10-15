a39c565bf565120ca9edf8688976a06a02a7642f
nandotmbn <orlandopratama695@gmail.com>
2023-10-15 16:39:11 +0700

    fix

--- src/controllers/documents/get-by-id.documents.controllers.ts
+++ src/controllers/documents/get-by-id.documents.controllers.ts
@@ -2,11 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { Documents } from "../../models";
 import message from "../../views/message";
 
-async function getDocumentsById(req: Request, res: Response, next: NextFunction) {
+async function getDocumentsById(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
 	const documentId: string = (req.params.documentId as string) || "";
-	const isArchived: boolean = req.query.isArchived == "true" ? true : false;
 
-	const labs = await Documents.findOne({ _id: documentId, isArchived }).populate("author", "fullName _id");
+	const labs = await Documents.findOne({ _id: documentId }).populate(
+		"author",
+		"fullName _id"
+	);
 	if (!labs) {
 		return res.status(404).send(
 			message({
